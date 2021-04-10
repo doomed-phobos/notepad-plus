d4b14d4c70bf3dcdc89f0b74f62721715531b231
TheEKJ <the_ekj.2121@outlook.com>
2021-04-10 16:26:54 -0500

    Update main

--- src/os/main.cpp
+++ src/os/main.cpp
@@ -1,8 +1,47 @@
 #include "os.h"
 
+#include <iostream>
+
+class A
+{
+public:
+   A() {
+      utils::Slot_mem<A, void, int> a(this, &A::foo);
+
+      a(12);
+   }
+
+   void foo(int a) {
+      printf("FOO: %d\n", a);
+   }
+};
+
 int main()
 {
+   /*
    os::System* sys(os::System::GetInstance());
+   std::unique_ptr<os::Window> win(sys->createWindow(800, 400));
 
+   win->setTitle("notepad+");
+   win->setVisible(true);
+
+   os::EventQueue* queue = sys->eventQueue();
+
+   bool is_run = true;
+   while(is_run) {
+      os::Event ev;
+      queue->getEvents(ev, true);
+
+      switch(ev.type) {
+      case os::Event::MouseDown_Type:
+         break;
+      case os::Event::Close_Type:
+         is_run = false;
+         break;
+      }
+   }*/
+
+   A a;
+   
    return 0;
 }
