548b5a3423e19a0b24d86d49cb11e23c302a1c89
TheEKJ <the_ekj.2121@outlook.com>
2021-04-11 16:46:19 -0500

    Update utils

---
+++ src/utils/paths.h
@@ -0,0 +1,11 @@
+#ifndef _UTILS_PATHS_H
+#define _UTILS_PATHS_H
+#include <string>
+#include <vector>
+
+namespace utils
+{
+   typedef std::vector<std::string> paths;
+} // namespace utils
+
+#endif
---
+++ src/utils/signal.h
@@ -0,0 +1,78 @@
+#ifndef _UTILS_SIGNAL_H
+#define _UTILS_SIGNAL_H
+#include "slot.h"
+
+#include <vector>
+
+namespace utils
+{
+   template<typename R, typename... Args>
+   class Signal_base
+   {
+   public:
+      typedef Slot<R, Args...> slot_t;
+      typedef std::vector<slot_t*> list_t;
+
+      Signal_base() {}
+      ~Signal_base() {
+         disconnectAll();
+      }
+      
+      slot_t* addSlot(slot_t* slot) {
+         m_slots.push_back(slot);
+         return slot;
+      }
+
+      slot_t* connect(R(*function)(Args...)) {
+         return addSlot(new Slot_fun<R, Args...>(function));
+      }
+
+      template<class T>
+      slot_t* connect(T* t, R(T::*function)(Args...)) {
+         return addSlot(new Slot_mem<T, R, Args...>(t, function));
+      }
+
+      void disconnectAll() {
+         for(typename list_t::iterator it = m_slots.begin();
+             it != m_slots.end(); it++)
+             delete *it;
+
+         m_slots.clear();
+      }
+
+   protected:
+      list_t m_slots;
+   };
+
+   template<typename R, typename... Args>
+   class Signal : public Signal_base<R, Args...>
+   {
+   public:
+      R operator()(Args&&... args) {
+         R result = R();
+
+         for(typename Signal_base<R, Args...>::list_t::iterator it = m_slots.begin();
+             it != m_slots.end(); it++) {
+            slot_t* slot = *it;
+            result = (*slot)(std::forward<Args>(args)...);
+         }
+
+         return result;
+      }
+   };
+
+   template<typename... Args>
+   class Signal<void, Args...> : public Signal_base<void, Args...>
+   {
+   public:
+      void operator()(Args&&... args) {
+         for(typename Signal_base<void, Args...>::list_t::iterator it = m_slots.begin();
+             it != m_slots.end(); it++) {
+            slot_t* slot = *it;
+            (*slot)(std::forward<Args>(args)...);
+         }
+      }
+   };
+} // namespace utils
+
+#endif
