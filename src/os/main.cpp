091e891b08ad45eec6c7e2bee30d75fa47c7c5d8
TheEKJ <the_ekj.2121@outlook.com>
2021-04-11 19:19:06 -0500

    Update main

--- src/os/main.cpp
+++ src/os/main.cpp
@@ -2,9 +2,49 @@
 
 #include <iostream>
 //TODO: Crear fuentes y surface::drawText
+
+static bool isFocus = false;
+
+struct Theme
+{
+   gfx::color_t background = gfx::colors::None;
+   gfx::color_t menu       = gfx::colors::None;
+   gfx::color_t page       = gfx::colors::None;
+
+   static Theme Focus() {
+      Theme theme;
+      theme.background = gfx::colors::Red;
+      theme.menu = gfx::rgba(240, 240, 240);
+      //theme.page = gfx::rgba(255, 255, 255);
+
+      return theme;
+   }
+
+   static Theme NoFocus() {
+      Theme theme;
+      theme.background = gfx::colors::Blue;
+      theme.menu = gfx::rgba(240, 240, 240);
+      //theme.page = gfx::rgba(255, 255, 255);
+
+      return theme;
+   }
+};
+
 void paint(os::Surface* surface)
 {
-   surface->clear(gfx::rgba(86, 157, 229));
+   Theme theme;
+   if(isFocus)
+      theme = Theme::Focus();
+   else
+      theme = Theme::NoFocus();
+
+   surface->clear(theme.background);
+
+   surface->drawRect(gfx::RectF(gfx::PointF(0, 0), gfx::PointT(surface->width(), 20)),
+      gfx::Paint(theme.menu));
+   
+   surface->drawRect(gfx::RectF(gfx::PointF(1, 21), gfx::PointF(surface->width()-1, surface->height()-1)),
+      gfx::Paint(theme.page));
 }
 
 int main()
@@ -25,11 +65,12 @@ int main()
       queue->getEvents(ev, true);
 
       switch(ev.type) {
-      case os::Event::KeyDown_Type:
-         std::wcout << (wchar_t)ev.unicodeChr;
-         break;
       case os::Event::Close_Type:
          is_run = false;
+         break;
+      case os::Event::Focus_Type:
+         isFocus = ev.isFocus;
+         win->invalidate();
          break;
       }
    }
