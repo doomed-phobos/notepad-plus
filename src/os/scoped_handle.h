3fc0564bb0ff0e345cc1afe19b7d59b71bb96cac
TheEKJ <the_ekj.2121@outlook.com>
2021-04-17 18:06:16 -0500

    Change unique_ptr to shared_ptr

--- src/os/scoped_handle.h
+++ src/os/scoped_handle.h
@@ -1,6 +1,5 @@
 #ifndef _OS_SCOPED_HANDLE_H
 #define _OS_SCOPED_HANDLE_H
-#include <atomic>
 #include <utility>
 
 namespace os
@@ -9,13 +8,20 @@ namespace os
    class ScopedHandle
    {
    public:
-      ScopedHandle(T* ptr = nullptr) : m_ptr(ptr), m_refCount(new RefCounter) {}
+      ScopedHandle() : m_ptr(nullptr), m_refCount(nullptr) {}
+      ScopedHandle(T* ptr) : m_ptr(ptr),
+                             m_refCount(new RefCounter) {}
+      
       ScopedHandle(const ScopedHandle& that) {
-         m_ptr = that.get();
+         m_ptr = that.m_ptr;
          m_refCount = that.m_refCount;
          ref();
       }
-      ScopedHandle(ScopedHandle&& that) : m_ptr(that.release()) {}
+
+      ScopedHandle(ScopedHandle&& that) {
+         m_ptr = that.release();
+         m_refCount = std::exchange(that.m_refCount, nullptr);
+      }
 
       ~ScopedHandle() {unref();}
 
@@ -24,25 +30,33 @@ namespace os
       }
 
       void reset(T* ptr = nullptr) {
-         if(m_ptr != ptr) {
-            m_ptr = ptr;
-            unref();
-         }
+         if(m_ptr != ptr)
+            ScopedHandle(ptr).swap(*this);
+      }
+
+      void swap(ScopedHandle& that) {
+         std::swap(m_ptr, that.m_ptr);
+         std::swap(m_refCount, that.m_refCount);
       }
 
       int use_count() const {
-         return m_refCount->get();
+         return m_refCount ? m_refCount->get() : 0;
       }
       
       T* get() const {return m_ptr;}
       T* operator->() const {return m_ptr;}
+      operator bool() const {return m_ptr != nullptr;}
 
       ScopedHandle& operator=(const ScopedHandle& that) {
-         if(m_ptr != that.m_ptr) {
-            reset(that.m_ptr);
-            m_refCount = that.m_refCount;
-            ref();
-         }
+         if(m_ptr != that.m_ptr)
+            ScopedHandle(that).swap(*this);
+
+         return *this;
+      }
+
+      ScopedHandle& operator=(ScopedHandle&& that) {
+         if(m_ptr != that.m_ptr)
+            ScopedHandle(std::move(that)).swap(*this);
 
          return *this;
       }
@@ -52,33 +66,33 @@ namespace os
          RefCounter() : m_refCount(1) {}
 
          int add() const {
-            return m_refCount.fetch_add(1, std::memory_order_relaxed);
+            return m_refCount++;//m_refCount.fetch_add(1, std::memory_order_relaxed);
          }
 
          int sub() const {
-            return m_refCount.fetch_add(-1, std::memory_order_relaxed);
+            return m_refCount--;//m_refCount.fetch_add(-1, std::memory_order_relaxed);
          }
 
          int get() const {
-            return m_refCount.load(std::memory_order_acquire);
+            return m_refCount;//m_refCount.load(std::memory_order_acquire);
          }
       private:
-         mutable std::atomic_int m_refCount;
+         mutable int m_refCount;
       };
 
       void ref() {
-         m_refCount->add();
+         if(m_refCount)
+            m_refCount->add();
       }
 
       void unref() {
-         if(m_refCount->sub() == 1)
-            safe_dispose();
-      }
-
-      inline void safe_dispose() {
-         delete m_refCount;
-         if(m_ptr)
+         if(m_refCount && m_refCount->sub() == 1) {
             delete m_ptr;
+            delete m_refCount;
+
+            m_ptr = nullptr;
+            m_refCount = nullptr;
+         }
       }
 
       RefCounter* m_refCount;
