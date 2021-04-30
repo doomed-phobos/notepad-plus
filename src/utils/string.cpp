0d02f547e9dbf7f84d7a679449edbe15f6916b1a
TheEKJ <the_ekj.2121@outlook.com>
2021-04-30 17:33:38 -0500

    Update string

--- src/utils/string.cpp
+++ src/utils/string.cpp
@@ -1,10 +1,25 @@
 #include "string.h"
 
+#include <cstdarg>
 #include <vector>
 #include <windows.h>
 
 namespace utils
 {
+   std::string format_to_string(const char* format, ...)
+   {
+      va_list args;
+      va_start(args, format);
+      std::size_t required_size = vsnprintf(nullptr, 0, format, args);
+      std::vector<char> buf(++required_size);
+
+      vsnprintf(&buf[0], buf.size(), format, args);
+      
+      va_end(args);
+
+      return std::string(&buf[0]);
+   }
+
    std::string to_utf8(const std::wstring& wstr)
    {
       int required_size = ::WideCharToMultiByte(
--- src/utils/string.h
+++ src/utils/string.h
@@ -4,6 +4,8 @@
 
 namespace utils
 {
+   std::string format_to_string(const char* format, ...);
+
    std::string to_utf8(const std::wstring& wstr);
    std::wstring from_utf8(const std::string& str);
 } // namespace utils
